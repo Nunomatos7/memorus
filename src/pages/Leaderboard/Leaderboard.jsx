c8518319a3d4eb8b035cd69a207f2406beb6e659
Nunomatos7 <nuno.esteves@ua.pt>
2025-04-15 19:34:17 +0100

    style(Leaderboard, Memors): add margin-top to header and card styles for improved spacing

--- src/pages/Leaderboard/Leaderboard.jsx
+++ src/pages/Leaderboard/Leaderboard.jsx
@@ -198,6 +198,7 @@ const Leaderboard = () => {
               fontWeight: "bold",
               color: "white",
               marginBottom: "10px",
+              marginTop: "32px",
             }}
           >
             Leaderboard
